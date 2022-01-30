364fa91dd56ebd6e5b0a2f291e343523fd1294dc
mrdrmitten <rayxmagner1@gmail.com>
2022-01-30 14:10:02 -0600

    Mongoose Part 2

--- index.js
+++ index.js
@@ -10,27 +10,25 @@ const connect = mongoose.connect(url, {
 
 connect.then(() => {
 
-    console.log('Connected correctly to server');
+  console.log('Connected correctly to server');
 
-    const newCampsite = new Campsite({
-        name: 'React Lake Campground',
-        description: 'test'
-    });
-
-    newCampsite.save()
-    .then(campsite => {
-        console.log(campsite);
-        return Campsite.find();
-    })
-    .then(campsites => {
-        console.log(campsites);
-        return Campsite.deleteMany();
-    })
-    .then(() => {
-        return mongoose.connection.close();
-    })
-    .catch(err => {
-        console.log(err);
-        mongoose.connection.close();
-    });
+  Campsite.create({
+    name: 'React Lake Campground',
+    description: 'test'
+  })
+  .then(campsite => {
+      console.log(campsite);
+      return Campsite.find();
+  })
+  .then(campsites => {
+      console.log(campsites);
+      return Campsite.deleteMany();
+  })
+  .then(() => {
+      return mongoose.connection.close();
+  })
+  .catch(err => {
+      console.log(err);
+      mongoose.connection.close();
+  });
 });
