c5bc02ab72675bed41e81b6461482a1bfb5bb427
mrdrmitten <rayxmagner1@gmail.com>
2022-01-30 14:23:49 -0600

    Mongoose Part 3

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Campsite = require('./models/campsite');
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -13,15 +14,31 @@ connect.then(() => {
   console.log('Connected correctly to server');
 
   Campsite.create({
-    name: 'React Lake Campground',
-    description: 'test'
+      name: 'React Lake Campground',
+      description: 'test'
   })
   .then(campsite => {
       console.log(campsite);
-      return Campsite.find();
+
+      return Campsite.findByIdAndUpdate(campsite._id, {
+          $set: { description: 'Updated Test Document' }
+      }, {
+          new: true
+      });
   })
-  .then(campsites => {
-      console.log(campsites);
+  .then(campsite => {
+      console.log(campsite);
+
+      campsite.comments.push({
+          rating: 5,
+          text: 'What a magnificent view!',
+          author: 'Tinus Lorvaldes'
+      });
+
+      return campsite.save();
+  })
+  .then(campsite => {
+      console.log(campsite);
       return Campsite.deleteMany();
   })
   .then(() => {
@@ -31,4 +48,4 @@ connect.then(() => {
       console.log(err);
       mongoose.connection.close();
   });
-});
+}) ;
